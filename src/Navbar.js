1bac4c23193b1e83b7180b7fc8f80ff3c06d3ec8
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-09-02 19:14:11 +0100

    useEffect conditional statement to toggle nav

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,10 +9,14 @@ const Navbar = () => {
   const linksRef = useRef(null);
 
   useEffect(() => {
-    const linksHeight = linksRef.current.getBoundingClientRect();
-  
-  }, [showLinks])
-  
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
+
   return (
     <nav>
       <div className="nav-center">
@@ -26,7 +30,7 @@ const Navbar = () => {
           </button>
         </div>
         <div className="links-container" ref={linksContainerRef}>
-          <ul className="links" ref = {linksRef}>
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link;
               return (
